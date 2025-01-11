651ef3a0d4c6bcd625aac5c596f22148f166ed76
Franco <alonsofranco1999@gmail.com>
2025-01-11 18:40:27 -0300

    Refactor StartupPage component: Remove EventCard instances and clean up layout

--- src/app/(routes)/resources/page.tsx
+++ src/app/(routes)/resources/page.tsx
@@ -1,33 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { ResourceCard } from "@/components/ui/cards/Resources";
 import Link from "next/link";
 
 export default function StartupPage() {
   return (
     <div>
-    <main className="flex-1 p-6 overflow-y-auto">
-   <div className="space-y-12">
-     {/* Startups Preview */}
-     <section>
-       <div className="flex items-center justify-between mb-6">
-         <h2 className="text-2xl font-bold">Startups Destacadas</h2>
-         <Button variant="outline" asChild>
-           <Link href="https://www.google.com">
-             + Agregar Venture 
-           </Link>
-         </Button>
-       </div>
-       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 grid-cols-1">
-         <ResourceCard type="book"/>
-         <ResourceCard type="book"/>
-         <ResourceCard type="book"/>
-         <ResourceCard type="book"/>
-         <ResourceCard type="book"/>
-         
-       </div>
-     </section>
-   </div>
- </main>
- </div>
+      <main className="flex-1 p-6 overflow-y-auto">
+        <div className="space-y-12">
+          {/* Startups Preview */}
+          <section>
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-2xl font-bold">Startups Destacadas</h2>
+              <Button variant="outline" asChild>
+                <Link href="https://www.google.com">+ Agregar Venture</Link>
+              </Button>
+            </div>
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 grid-cols-1"></div>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
