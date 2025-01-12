55f2b0880b544f172b33ced5a88ea0003c1d4be6
Franco <alonsofranco1999@gmail.com>
2025-01-11 22:25:51 -0300

    Update package dependencies and enhance StartupPage functionality: - Added @radix-ui/react-dialog and @radix-ui/react-label for improved UI components. - Implemented a dialog for adding new startups with input fields for name and description. - Adjusted layout in FavoritesPage for better responsiveness. - Minor style adjustments in button component.

--- src/app/(routes)/favorites/page.tsx
+++ src/app/(routes)/favorites/page.tsx
@@ -12,7 +12,7 @@ export default function FavoritesPage() {
     <main className="flex-1 p-6 overflow-y-auto">
       <div className="space-y-11">
      <h2 className="text-2xl font-bold">Mis Favoritos</h2>
-      <div className="grid gap-6 grid-cols-3 ">
+      <div className="grid gap-6 md:grid-cols-3 grid-cols-1 ">
         {favoriteStartups.length > 0 ? (
           favoriteStartups.map((startup) => (
             <StartupCard key={startup.id} startup={startup} />
--- src/app/(routes)/startups/page.tsx
+++ src/app/(routes)/startups/page.tsx
@@ -1,11 +1,25 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import StartupGrid from "@/components/ui/gridElements/StartupGrid";
 import { startups } from "@/seed/seed";
-import Link from "next/link";
+import { useState } from "react";
 
 const startupsPage = startups;
 
 export default function StartupPage() {
+  const [open, setOpen] = useState(false);
+
   return (
     <main className="flex-1 p-6 overflow-y-auto">
       <div className="space-y-12">
@@ -13,9 +27,44 @@ export default function StartupPage() {
         <section>
           <div className="flex items-center justify-between mb-6">
             <h2 className="text-2xl font-bold">Startups Destacadas</h2>
-            <Button variant="outline" asChild>
-              <Link href="https://www.google.com">+ Agregar Startup</Link>
-            </Button>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogTrigger asChild>
+                <Button variant="outline">+ Agregar Startup</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Agregar Startup</DialogTitle>
+                  <DialogDescription>
+                    Ingrese los detalles de la nueva startup aquí. Haga clic en
+                    guardar cuando haya terminado.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Nombre
+                    </Label>
+                    <Input id="name" className="col-span-3" />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="description" className="text-right">
+                      Web o Red social
+                    </Label>
+                    <Input id="description" className="col-span-3" />
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button
+                    type="submit"
+                    variant={"default"}
+                    className="bg-blue-500 hover:bg-blue-600"
+                    onClick={() => setOpen(false)}
+                  >
+                    Enviar
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </div>
           <div>
             <StartupGrid startups={startupsPage} />
